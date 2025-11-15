7a20aafaafbe562236769546d870b358bc7fcef7
Luciana Velasco <luwuvp22@gmail.com>
2025-11-15 02:54:52 -0400

    Corregir warning de PHP 7.4: inicializar resultMatrixReport como stdClass

--- config.inc.php
+++ config.inc.php
@@ -582,6 +582,8 @@ $tlCfg->reportsCfg->start_time = '00:00';
 
 // Result matrix (resultsTC.php)
 // Shows an extra column which gives the status of the last executed build
+$tlCfg->resultMatrixReport = new stdClass();
+$tlCfg->resultMatrixReport->buildColumns = new stdClass();
 $tlCfg->resultMatrixReport->buildColumns['showStatusLastExecuted'] = true;
 
 // Show build columns in revers order. The latest build is to the left
